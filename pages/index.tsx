ba3946e233e0ecce153f1e0da94bff48b59ee8dd
Mateusz <77123763+0xpako@users.noreply.github.com>
2023-05-18 17:09:32 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Image } from "../lib/components/Image";
 export default function Index() {
   return (
     <Layout>
+      <title>Emage Starter</title>
       <div className="flex flex-col items-center justify-center">
         <main className="flex flex-col items-center justify-between pt-10">
           <div className="flex flex-col items-center flex-1 px-4 sm:px-20 text-center z-10 sm:pt-10 mb-16">
