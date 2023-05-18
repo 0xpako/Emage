1bdfdc287eece2bbfd0c83657d92e34b034d24fa
Mateusz <77123763+0xpako@users.noreply.github.com>
2023-05-18 08:04:05 -0700

    Add files via upload

---
+++ lib/index.ts
@@ -0,0 +1,70 @@
+import { join } from "path";
+import { copy, emptyDir, ensureDir, writeJSON } from "fs-extra";
+import { getTransformedRoutes } from "@vercel/routing-utils";
+
+import { getRoutes } from "./utils/routes";
+import { createStaticFile } from "./build/generate/createStaticFile";
+import { createServerlessFunction } from "./build/generate/createServerlessFunction";
+import { createPrerender } from "./build/generate/createPrerender";
+import { existsSync } from "fs";
+import { createEdgeFunction } from "./build/generate/createEdgeFunction";
+
+require("@babel/register")({
+  extensions: [".ts", ".tsx", ".js", ".jsx"],
+  presets: ["@babel/preset-env"],
+  plugins: [["@babel/plugin-transform-react-jsx", { runtime: "automatic" }]],
+});
+
+async function prepareFileSystem() {
+  await emptyDir(join(".vercel", "output"));
+
+  return Promise.allSettled([ensureDir(join(".vercel", "output", "static"))]);
+}
+
+async function buildVercelOutput() {
+  await prepareFileSystem();
+
+  try {
+    await Promise.allSettled(
+      getRoutes().map(async (filePath) => {
+        const { pageConfig, default: Component } = await import(filePath);
+
+        switch (pageConfig.strategy) {
+          case "static":
+            return createStaticFile(Component, filePath);
+          case "prerender":
+            return createPrerender(Component, filePath, pageConfig);
+          case "ssr":
+            return createServerlessFunction(Component, filePath);
+          case "edge":
+            return createEdgeFunction(Component, filePath);
+
+          default:
+            return;
+        }
+      })
+    );
+  } catch (e) {
+    console.log(e);
+  }
+
+  if (existsSync(join("public"))) {
+    await copy(join("public"), join(".vercel", "output", "static"));
+  }
+
+  return writeJSON(".vercel/output/config.json", {
+    ...(existsSync(process.cwd() + "/vercel.config.js")
+      ? require(process.cwd() + "/vercel.config.js")?.default
+      : {}),
+    ...{
+      version: 3,
+      routes: getTransformedRoutes({
+        nowConfig: { cleanUrls: true },
+      }).routes,
+    },
+  });
+}
+
+buildVercelOutput()
+  .then(() => console.log("✅ Succesfully created Build Output API folder"))
+  .catch((e) => console.log("❌ Failed to create Build Output API folder", e));
---
+++ vercel.config.js
@@ -0,0 +1,7 @@
+export default {
+  images: {
+    sizes: [450, 1125],
+    domains: ["cdn.shopify.com"],
+    formats: ["image/webp", "image/avif"],
+  },
+};
