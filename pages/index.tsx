e71c6a9d469b891da31c5adff56e0613636957d3
Mateusz <77123763+0xpako@users.noreply.github.com>
2023-05-18 18:01:22 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ export default function Index() {
     <Layout>
       <head>
       <title>Emage Starter</title>
+      <link rel="shortcut icon" href="/favicon256.png" />
       </head>
       <div className="flex flex-col items-center justify-center">
         <main className="flex flex-col items-center justify-between pt-10">
