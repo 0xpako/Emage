b4dd580e4c08f5d181426e4568d729e7fce41bf5
Mateusz <77123763+0xpako@users.noreply.github.com>
2023-05-19 01:13:56 +0200

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -11,16 +11,37 @@ export default function Nav() {
         className={cn("flex items-center lg:px-6 px-8 mx-auto max-w-7xl px-14")}
       >
         <div className="flex flex-row items-center">
+          <span>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="100%" height="100%" rx="16" fill="#000"></rect>
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+                fill="#fff"
+              ></path>
+            </svg>
+          </span>
           <ul className="flex items-center content-center">
             <li className="ml-2 text-gray-200">
               <a href="/">
-                <img
-                  src="./public/favicon256.png"
+                <svg
                   viewBox="0 0 24 24"
-                  width=32px
-                  height=32px
-                  
-                ></img>
+                  width="32"
+                  height="32"
+                  stroke="currentColor"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  shapeRendering="geometricPrecision"
+                ></svg>
               </a>
             </li>
             <li className="font-medium" style={{ letterSpacing: ".01px" }}>
