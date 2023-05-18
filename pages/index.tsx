2ea4d78544dab30ff8c961a7abf801f01ea72373
Mateusz <77123763+0xpako@users.noreply.github.com>
2023-05-19 00:58:05 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,10 @@ export default function Index() {
         <main className="flex flex-col items-center justify-between pt-10">
           <div className="flex flex-col items-center flex-1 px-4 sm:px-20 text-center z-10 sm:pt-10 mb-16">
             <h1 className="text-3xl sm:text-5xl font-bold text-center">
-              Shop the latest Acme merchandise
+              Here's where your app starts!
             </h1>
             <p className="mt-4 sm:text-xl text-lg text-gray-700">
-              Shop the latest Acme merch for the best price
+              Put anything you can e-magine here
             </p>
           </div>
           <Image src="/landing-white.png" height={600} width={750} />
