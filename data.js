5df882711c9ea25db4427e84c8ab2870a9a3b0a4
Mateusz <77123763+0xpako@users.noreply.github.com>
2023-05-19 02:38:11 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,30 +1,28 @@
 export default [
   {
     id: 1,
-    name: "Orange Art 1",
+    name: "Orange Art One",
     price: 150,
     color: "white",
-    src: "https://i.ibb.co/4mF5R2w/short-sleeve-t-shirt-0-1.webp",
+    src: "https://raw.githubusercontent.com/0xpako/Emage/main/public/artone.webp",
   },
   {
     id: 2,
-    name: "Orange Art 2",
+    name: "Orange Art Two",
     price: 135,
-    color: "white",
-    src: "https://i.ibb.co/xXbc4B0/short-sleeve-t-shirt-0-2.webp",
+    src: "https://raw.githubusercontent.com/0xpako/Emage/main/public/arttwo.webp",
   },
   {
     id: 3,
-    name: "Orange Art 3",
+    name: "Orange Art Three",
     price: 120,
-    color: "white",
-    src: "https://i.ibb.co/f4gSjXW/short-sleeve-t-shirt-0-3.webp",
+    src: "https://raw.githubusercontent.com/0xpako/Emage/main/public/artthree.webp",
   },
   {
     id: 4,
-    name: "Orange Art 4",
+    name: "Orange Art Four",
     price: 70,
     color: "white",
-    src: "https://i.ibb.co/6m7mBq1/short-sleeve-t-shirt-0-4.webp",
+    src: "https://raw.githubusercontent.com/0xpako/Emage/main/public/artfour.webp",
   },
 ];
